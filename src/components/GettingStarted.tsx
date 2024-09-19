87692e354a3a4e79bdeba4e83727ba7e824ab619
Harsh Manwani <invokerharsh@gmail.com>
2024-09-20 02:25:24 +0530

    add collapsible card and event table in getting started page

--- src/components/GettingStarted.tsx
+++ src/components/GettingStarted.tsx
@@ -1,20 +1,41 @@
-import ActionCard from "./ActionCard";
+import CollapsibleCard from "./CollapsibleCard";
+import EventTable from "./EventTable";
 
 export default function GettingStarted() {
   return (
     <div className="space-y-4">
-      <ActionCard
-        title="Install Surface Tag on your site"
+      <CollapsibleCard
+        title="Install the Surface Tag"
         description="Enable tracking and analytics."
-        actionText="Install tag"
-        actionVariant="primary"
-      />
-      <ActionCard
+      >
+        <pre className="whitespace-pre-wrap">
+          <code>
+            {`<script>
+(function(w, d, s, l, i) {
+  w[l] = w[l] || [];
+  w[l].push({
+    'surface.start': new Date().getTime(),
+    event: 'surface.js'
+  });
+});
+var f = d.getElementsByTagName(s)[0],
+    j = d.createElement(s),
+    dl = l != 'surface' ? '&l=' + l : '';
+j.async = true;
+j.src = 'https://www.surface-analytics.com/tag.js?id=' + i + dl;
+f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'surface', 'SURFACE_TAG_ID');
+</script>`}
+          </code>
+        </pre>
+        <p className="mt-2 text-green-600">Connected successfully!</p>
+      </CollapsibleCard>
+      <CollapsibleCard
         title="Test Surface Tag Events"
         description="Test if the Surface Tag is properly emitting events."
-        actionText="Test Tag"
-        actionVariant="secondary"
-      />
+      >
+        <EventTable />
+      </CollapsibleCard>
     </div>
   );
 }
