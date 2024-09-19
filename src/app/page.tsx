971bf47d3eac313ebe37be4a2341f4a843a7f237
Harsh Manwani <invokerharsh@gmail.com>
2024-09-20 02:26:22 +0530

    remove width constrains on the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import GettingStarted from "../components/GettingStarted";
 
 export default function HomePage() {
   return (
-    <div className="max-w-4xl">
+    <div className="">
       <h1 className="mb-6 text-3xl font-bold">Getting started</h1>
       <GettingStarted />
     </div>
